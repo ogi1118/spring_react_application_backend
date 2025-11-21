bd6db24925e6be106ebca34d11514823e2f86511
ogi1118 <aratanew2525@gmail.com>
2025-11-22 06:52:16 +0900

    コメント間違ってて草

--- src/main/java/net/javaguides/spring_react_application_backend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/spring_react_application_backend/controller/EmployeeController.java
@@ -14,7 +14,7 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    //おそらくRequestMappingナシで@GetMapping("/api/employees/")でも可
+    //おそらくRequestMappingナシで@GePostMapping("/api/employees/")でも可
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);

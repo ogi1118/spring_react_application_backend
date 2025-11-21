27e7ba3a84c2f765f4a5c06e85b3120fbf0cce0b
ogi1118 <aratanew2525@gmail.com>
2025-11-21 22:41:36 +0900

    EmployeeのDtoクラス、Entity⇔Dto変換用マッパー作成

---
+++ src/main/java/net/javaguides/spring_react_application_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package net.javaguides.spring_react_application_backend.mapper;
+
+import net.javaguides.spring_react_application_backend.dto.EmployeeDto;
+import net.javaguides.spring_react_application_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee e){
+        return new EmployeeDto(
+                e.getId(),
+                e.getFirstName(),
+                e.getLastName(),
+                e.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto empDto){
+        return new Employee(
+                empDto.getId(),
+                empDto.getFirstName(),
+                empDto.getLastName(),
+                empDto.getEmail()
+        );
+    }
+}

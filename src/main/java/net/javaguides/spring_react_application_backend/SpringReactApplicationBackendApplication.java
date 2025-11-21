6e97af379c3b5d0c4181bc22b0cb643c86a8b382
ogi1118 <aratanew2525@gmail.com>
2025-11-21 20:05:15 +0900

    create spring boot project

---
+++ src/main/java/net/javaguides/spring_react_application_backend/SpringReactApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.spring_react_application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/spring_react_application_backend/SpringReactApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.spring_react_application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

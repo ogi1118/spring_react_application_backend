361f488348d8df98ae081afb94e76b5aaf9b8c36
ogi1118 <aratanew2525@gmail.com>
2025-11-24 00:28:37 +0900

    Httpメソッド、APIエンドポイントごとに認可設定

--- src/main/java/net/javaguides/spring_react_application_backend/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/spring_react_application_backend/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package net.javaguides.spring_react_application_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -24,9 +25,16 @@ public class SpringSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((authorize) ->
-                        authorize.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests((authorize) -> {
+                    // 指定したpathに対するPOSTに対しては指定したロールでしかアクセスできないようにする(認可)
+                    // 今回はADMINユーザーのみEmployeeの編集ができるように設定(USERは閲覧のみ)
+                    authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 

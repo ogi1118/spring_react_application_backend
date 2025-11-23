2c4e1a1332b79abe27f3edad9c7730f411d62404
ogi1118 <aratanew2525@gmail.com>
2025-11-24 01:14:01 +0900

    コメントを追加

--- src/main/java/net/javaguides/spring_react_application_backend/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/spring_react_application_backend/config/SpringSecurityConfig.java
@@ -3,6 +3,8 @@ package net.javaguides.spring_react_application_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,6 +32,8 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests((authorize) -> {
                     // 指定したpathに対するPOSTに対しては指定したロールでしかアクセスできないようにする(認可)
                     // 今回はADMINユーザーのみEmployeeの編集ができるように設定(USERは閲覧のみ)
+                    // Role Hierarchyを設定すれば、階層型の権限設定ができる
+                    // 現在の実装だと権限は独立しているのでhasAnyRoleで列挙して指定
 //                    authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll(); // 開発用
                     authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
                     authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");

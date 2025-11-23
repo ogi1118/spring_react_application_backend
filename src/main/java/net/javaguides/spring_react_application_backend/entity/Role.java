d012e2264199639cdb66bf7da5e15b890e6f37af
ogi1118 <aratanew2525@gmail.com>
2025-11-24 01:37:28 +0900

    ユーザー、権限のエンティティを作成

---
+++ src/main/java/net/javaguides/spring_react_application_backend/entity/Role.java
@@ -0,0 +1,22 @@
+package net.javaguides.spring_react_application_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
---
+++ src/main/java/net/javaguides/spring_react_application_backend/entity/User.java
@@ -0,0 +1,42 @@
+package net.javaguides.spring_react_application_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany/*多対多を表す(中間テーブルも自動生成、変数の型を参照して自動でどのエンティティと紐づけるかまで拾う)*/(
+            fetch = FetchType.EAGER /*データ取得タイミング(EAGER：即時)*/,
+            cascade = CascadeType.ALL /*紐づいたエンティティにした操作が紐づけ先にも同様に行われる(ALL：全ての操作)*/)
+    @JoinTable(/*中間テーブルの詳細設定(無い場合は自動で規則的に決まる)*/
+            name = "users_roles",
+            joinColumns = @JoinColumn/*これが記述されている側のカラム*/(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn/*紐づけ先のカラム*/(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles; // 1人が複数ロール持つこともある(権限は独立している設計のため)
+}
